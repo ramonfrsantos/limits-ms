1affd067632da8b23cf82de1f304a71a38d0f21c
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-19 18:52:23 -0300

    finishing naming server with Eureka and Feign

--- currency-conversion-service/src/main/java/br/com/my/microservices/currencyconversionservice/bean/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/br/com/my/microservices/currencyconversionservice/bean/CurrencyExchangeProxy.java
@@ -4,7 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+// Feign através do Eureka produz load balancer com spring cloud,
+// descartando o uso de um hardcoded url
+
+// @FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
   
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
