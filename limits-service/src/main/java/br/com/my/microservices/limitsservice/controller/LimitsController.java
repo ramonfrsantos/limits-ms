68aa88aa77e7f0b30f571a23fc86056357a6bb4c
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-19 00:18:27 -0300

    creating project

---
+++ limits-service/src/main/java/br/com/my/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package br.com.my.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.my.microservices.limitsservice.bean.Limits;
+import br.com.my.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+  @Autowired
+  private Configuration configuration;
+  
+  @GetMapping("/limits")
+  public Limits retrieveLimits(){
+    return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    // return new Limits(1, 100);
+  }
+}
