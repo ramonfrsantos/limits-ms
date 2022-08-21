1724820c3dbd86d67aa35d00b6a83f47c7ce9f48
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-20 23:34:46 -0300

    adding zipkin to the project

--- currency-exchange-service/src/main/java/br/com/my/microservices/currencyexchangeservice/service/CurrencyExchangeService.java
+++ currency-exchange-service/src/main/java/br/com/my/microservices/currencyexchangeservice/service/CurrencyExchangeService.java
@@ -2,6 +2,8 @@ package br.com.my.microservices.currencyexchangeservice.service;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import br.com.my.microservices.currencyexchangeservice.repository.CurrencyExchan
 
 @Service
 public class CurrencyExchangeService {
+
+  private Logger logger = LoggerFactory.getLogger(CurrencyExchangeService.class);
   @Autowired
   private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -20,6 +24,8 @@ public class CurrencyExchangeService {
 
   public CurrencyExchange retrieveExchangeValue(String from, String to){
     String port = environment.getProperty("local.server.port");
+
+    logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
     CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
     if(Objects.isNull(currencyExchange)){
